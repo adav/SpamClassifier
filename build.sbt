1f04df96ce977b0f0a83555e63a56c8c0abc127c
Andrew Davidson <andrew.davidson@mendeley.com>
2017-03-15 16:48:19 +0000

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "SpamClassifier"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.1.0"
+libraryDependencies += "org.apache.spark" % "spark-mllib_2.11" % "2.1.0"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,53 @@
+import org.apache.spark.mllib.classification.NaiveBayes
+import org.apache.spark.mllib.feature.HashingTF
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Main extends App {
+
+  val conf = new SparkConf()
+    .setAppName("DatasetSpam")
+    .setMaster("local")
+
+  val sc = new SparkContext(conf)
+
+  val hashingTf = new HashingTF(1000)
+
+  val data = sc.textFile("dataset_spam.csv").map { line =>
+    line.split(",").toList match {
+      case "0" :: _ => LabeledPoint(0.0, hashingTf.transform(line.substring(2).split(" ")))
+      case "1" :: _ => LabeledPoint(1.0, hashingTf.transform(line.substring(2).split(" ")))
+    }
+  }
+
+
+  val Array(training, test) = data.randomSplit(Array(0.6, 0.4))
+
+  val model = NaiveBayes.train(training, lambda = 1.0)
+
+  val predictionAndLabel = test.map(p => (model.predict(p.features), p.label))
+
+  val accuracy = 1.0 * predictionAndLabel.filter(x => x._1 == x._2).count() / test.count()
+
+  println(s"accuracy $accuracy")
+
+  //------
+
+  val madeUpSpam = List(
+    "watch lemons in hyderabad",
+    "comic radiation dataset",
+    "molecular cell 233",
+    "bananas",
+    "washing machine repairs",
+    "buy washing",
+    "washing repairs",
+    "buy washing machine",
+    "watch logan now"
+
+  )
+  val testSpam = madeUpSpam.map(name => (name, hashingTf.transform(name.mkString.split(" "))))
+  testSpam.foreach {
+    case (name, prediction) => println(s"$name ===> ${model.predict(prediction)}")
+  }
+
+}
